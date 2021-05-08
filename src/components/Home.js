7b74ab05b0af54561e1ca38063713a1372a630d4
Deepak Biswal <dbiswal@ipost.com>
2021-05-08 21:35:01 +0530

    Removed unused dependency import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { Form, Button, Row, Col, Card } from 'react-bootstrap';
 
 import Notification from '../components/Notification';
