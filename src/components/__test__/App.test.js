3afd37acdb92f0df36f65d3e8b9518e8bd3a5aae
Deepak Biswal <dbiswal@ipost.com>
2021-05-08 23:48:12 +0530

    Fix test case issues for App and NavBar component

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -3,12 +3,8 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App Component', () => {
-	beforeAll(() => {
-		render(<App />);
-	});
-
 	it('renders website name', () => {
 		render(<App />);
-		//expect(screen.getByText('MEETUP PORTAL')).toBeInTheDocument();
+		expect(screen.getByText('MEETUP PORTAL')).toBeInTheDocument();
 	});
 });
--- src/components/__test__/NavBar.test.js
+++ src/components/__test__/NavBar.test.js
@@ -3,20 +3,18 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('NavBar Component', () => {
-	beforeAll(() => {
-		render(<App />);
-	});
-
 	it('Find Home menu', () => {
-		//expect(screen.getByText('Home')).toBeInTheDocument();
 		render(<App />);
+		expect(screen.getByText('Home')).toBeInTheDocument();
 	});
 
-	// it('Find Admin menu', () => {
-	// 	expect(screen.getByText('Admin')).toBeInTheDocument();
-	// });
+	it('Find Admin menu', () => {
+		render(<App />);
+		expect(screen.getByText('Admin')).toBeInTheDocument();
+	});
 
-	// it('Find Report menu', () => {
-	// 	expect(screen.getByText('Admin')).toBeInTheDocument();
-	// });
+	it('Find Report menu', () => {
+		render(<App />);
+		expect(screen.getByText('Admin')).toBeInTheDocument();
+	});
 });
